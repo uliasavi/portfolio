533cd13e9cd81e3a599c268486b175c970a0f568
uliasavi <63160106+uliasavi@users.noreply.github.com>
2020-04-27 13:04:04 +0300

    portfolio

--- Prepros Export/js/burger-script.js
+++ Prepros Export/js/burger-script.js
@@ -3,4 +3,10 @@ $(function(){
 			$('.header__burger,.header__item').toggleClass('active');
 			$('body').toggleClass('lock');
 	});
+
+
+	$('.header__link.main').click(function(event){
+			$('.header__burger,.header__item').toggleClass('active');
+				$('body').removeClass('lock');
+	});
 });
